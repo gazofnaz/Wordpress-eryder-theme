46d19c2cf7b7459e570af74bde71023cf577c7a3
Gareth Arnott <gareth.arnott@informa.com>
2014-10-12 09:53:26 +0100

    fix rows and whitespace on home page

--- index.php
+++ index.php
@@ -1,71 +1,66 @@
-<?php get_header(); ?>
+<?php
 
-    	<?php if( !is_single() 
-                  && !is_page() 
-                  && !is_front_page()) : ?>
-                <h1><?php wp_title(' ', true, 'right'); ?></h1>
-        <?php endif; ?>
-        
-        <?php if (have_posts()) : ?>
+    get_header();
 
-        <div class="ga-box-list">
+?>
 
-		<?php 
+<div class="row">
+    <h1><?php wp_title(' ', true, 'right'); ?></h1>
+</div>
+<?php if (have_posts()) : ?>
+<div class="row">
+    <div class="ga-box-list">
 
-            $count = 0;
-            while (have_posts()) : the_post(); 
-            $count ++;
-            $post_image_src = get_post_custom()['wpcf-post-image'][0];
+<?php 
 
-        ?>
-            <div class="col-md-4 col-sm-11 no-left-gutter">
-                <!-- first item always has "no-left-gutter"-->
-                <div class="box-item pull-left col-sm-11 col-xs-11">
-                    <p class="ga-box-description">
-                        <h4>
-                            <a href="<?php the_permalink() ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h4>
-                    </p>
-                    <a href="<?php the_permalink() ?>">
-                        <img src="<?php echo $post_image_src ?>" class="img-responsive"/>
-                    </a>
-                    <div class="clearfix"></div>
-                    <p class="ga-box-description">
-                        <?php the_content('Read more &gt;'); ?>
-                    </p>
-                    <div class="ga-box-links" >
-                        <p class="text-center">
-                            <a href="<?php the_permalink() ?>">Find out more...
+    $count = 0;
+    while (have_posts()) : the_post(); 
+    $count ++;
+    $post_image_src = get_post_custom()['wpcf-post-image'][0];
+
+?>
+        <div class="col-md-4 col-sm-11 no-left-gutter">
+            <!-- first item always has "no-left-gutter"-->
+            <div class="box-item pull-left col-sm-11 col-xs-11">
+                <p class="ga-box-description">
+                    <h4>
+                        <a href="<?php the_permalink() ?>">
+                            <?php the_title(); ?>
                         </a>
-                        </p>
-                    </div>
+                    </h4>
+                </p>
+                <a href="<?php the_permalink() ?>">
+                    <img src="<?php echo $post_image_src ?>" class="img-responsive"/>
+                </a>
+                <div class="clearfix"></div>
+                <p class="ga-box-description">
+                    <?php the_content('Read more &gt;'); ?>
+                </p>
+                <div class="ga-box-links" >
+                    <p class="text-center">
+                        <a href="<?php the_permalink() ?>">Find out more...
+                    </a>
+                    </p>
                 </div>
             </div>
-        <?php 
-
-            /* fix for variable height items */
-            if( $count % 3 == 0 ){
-                echo "<div class='clearfix'></div>";
-            }
-
-        ?>
-        
-        <?php endwhile; ?>
-
-        </div><!-- ga-box-list -->
-        
-        <!-- @todo previous / next post buttons are nice if they have the actual post name in the link -->
-        
-        <?php else : /* show page not found message */ ?>
-        <!-- @todo add a 404 page -->
-        <div>
-            <h1>Page not found</h1>
-            <p>Sorry, the page you are looking for is not available. It may have moved, or you may have followed a bad link. Please 
-            <a href="<?php bloginfo('url') ?>">visit our homepage</a> to find what you're looking for.</p>
         </div>
-        
-        <?php endif; /* end if have_posts */ ?>      
+<?php 
+
+    /* fix for variable height items */
+    if( $count % 3 == 0 ){
+        echo "<div class='clearfix'></div>";
+    }
+
+?>
+
+<?php endwhile; ?>
+    </div><!-- ga-box-list -->
+</div>
+
+<?php endif; /* end if have_posts */ ?>      
     
-<?php get_footer(); ?>
+<?php
+
+   get_footer();
+
+?>
--- page.php
+++ page.php
@@ -1,12 +1,25 @@
-<?php get_header(); ?>
-<div class="col-sm-12">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php
 
-    <h1><?php the_title(); ?></h1>
+    get_header();
 
-    <?php 
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+
+<?php 
         the_content();
         endwhile; endif;
-    ?>
+?>
+
+    </div>
 </div>
-<?php /* footer */ get_footer(); ?>
+
+<?php
+
+   get_footer();
+
+?>
