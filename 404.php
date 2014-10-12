1fbf9d116cc3ac2bbc28afd6b26fd96c484ccf34
Gareth Arnott <gareth.arnott@informa.com>
2014-10-12 09:59:35 +0100

    add 404 (not working locally), remove unused templates

---
+++ 404.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Page not found</h1>
+    <p>Sorry, the page you are looking for is not available. It may have moved, or you may have followed a bad link. Please 
+    <a href="<?php bloginfo('url') ?>">visit our homepage</a> to find what you're looking for.</p>
+</div>
