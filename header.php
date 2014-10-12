481a06bbf9d0756d770ad6832c53a1aa1d146069
Gareth Arnott <gareth.arnott@informa.com>
2014-10-12 16:20:07 +0100

    convert menu to full navbar

--- header.php
+++ header.php
@@ -28,20 +28,30 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <h1>
-                    <a href="<?php bloginfo('url'); ?>">
-                        <?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>
-                    </a>
-                </h1>
+                <nav class="navbar navbar-default">
+                    <div class="navbar-header"> 
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span> 
+                            <span class="icon-bar"></span> 
+                            <span class="icon-bar"></span> 
+                            <span class="icon-bar"></span> 
+                        </button> 
+                        <a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+                            <?php bloginfo('name'); ?> - <small><?php bloginfo('description'); ?></small>
+                        </a>
+                    </div> 
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 <?php
-                $args = array(  'menu' => 'top_menu',
-                                'depth' => 2,
-                                'container' => false,
-                                'menu_class' => 'nav navbar-nav',
-                                'walker' => new wp_bootstrap_navwalker());
-                
-                wp_nav_menu($args);
+                    $args = array(  'menu' => 'top_menu',
+                                    'depth' => 2,
+                                    'container' => false,
+                                    'menu_class' => 'nav navbar-nav navbar-right',
+                                    'walker' => new wp_bootstrap_navwalker());
+
+                    wp_nav_menu($args);
 ?>
+                    </div>
+                </nav>
             </div>
         </div>
     </div>
